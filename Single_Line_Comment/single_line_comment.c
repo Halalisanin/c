73cdf4303f96f8735c26758f6e1311a3bbee72bf
Halalisanin <halalisanin@gmail.com>
2025-01-06 02:04:58 +0000

    propose

---
+++ Single_Line_Comment/single_line_comment.c
@@ -0,0 +1,10 @@
+// C program to illustrate
+// usde of single-line comment
+#include <stdio.h>
+
+int main(void)
+{
+	// This is a single-line comment
+	printf("Welcome to geeks of the south\n");
+	return 0;
+}
