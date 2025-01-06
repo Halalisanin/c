0872b549fd00ad32cd3345d6c69b9d2bdc04218d
Halalisanin <halalisanin@gmail.com>
2025-01-06 01:57:18 +0000

    light

---
+++ dif_dif/dif.c
@@ -0,0 +1,9 @@
+// C++ Program to illustrate the difference in different
+//  Compiler
+#include <stdio.h>
+int main()
+{
+	int i = 1;
+	printf("%d %d %d\n", i++, i++, i);
+	return 0;
+}
---
+++ putchar/putchar.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+	char *str = "Hello, World!\n";
+		while (*str) {
+			putchar(*str++);
+		}
+	return 0;
+}
